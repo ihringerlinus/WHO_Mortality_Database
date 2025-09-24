456b0b641d32f5ce125ce01826ccaa58701a4cf0
codingkr1s <suntev.kristian@gmail.com>
2025-09-24 14:56:13 +0200

    test

---
+++ Country_Code_Mapping.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+codes = pd.read_csv("country_codes.csv", encoding="utf-8-sig")
+mortality = pd.read_csv("Morticd10_part6.csv", encoding="utf-8-sig")
+merged = mortality.merge(codes, left_on="Country", right_on="country", how="left")
+merged["Country"] = merged["name"]
+merged = merged.drop(columns=["country", "name"])
+merged.to_csv("mortality_with_country_names.csv", index=False)
