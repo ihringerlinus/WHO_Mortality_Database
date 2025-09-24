727a368e5761d736c70231b61e1b09ff197a75f4
ihringerlinus <linus.ihringer@student.unisg.ch>
2025-09-24 16:16:04 +0200

    anpasstung gitignore und anpassungen jupiter notebook

--- Data_Files/Country_Code_Mapping.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-codes = pd.read_csv("../codes_Files/country_codes.csv", encoding="utf-8-sig")
-mortality = pd.read_csv("Morticd10_part6.csv", encoding="utf-8-sig")
-merged = mortality.merge(codes, left_on="Country", right_on="country", how="left")
-merged["Country"] = merged["name"]
-merged = merged.drop(columns=["country", "name"])
-merged.to_csv("mortality_with_country_names.csv", index=False)
